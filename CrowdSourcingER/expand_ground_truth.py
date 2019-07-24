e1a3c293670441d629b86b97a20eac66ba093383
AlessandroCimmino <ale.cimmino@stud.uniroma3.it>
2019-07-25 00:28:29 +0200

    Update expand_ground_truth.py

--- CrowdSourcingER/expand_ground_truth.py
+++ CrowdSourcingER/expand_ground_truth.py
@@ -5,6 +5,19 @@ from storing import mongoDB as md
 
 df = pd.read_csv(os.path.join(".","oracolo","true_predictions.csv"))
 
+spark = SparkSession.builder.appName('bigData')\
+                    .config("spark.mongodb.output.uri",config.MONGO_URI)\
+                    .getOrCreate()
+
+oracolo = spark.read.format("csv")\
+  .option("sep", "\t")\
+  .option("inferSchema", "true")\
+  .option("header", "true")\
+  .load("oracolo/true_predictions.csv")\
+  .rdd
+
+print("\n\nPRECISION:"+str(measures.precision(oracolo))+"%\n\n")
+
 for index,row in df.iterrows():
     if row["response"]==1:
         md.expand_ground_truth(row['ltable_id'],row['rtable_id'],"cameras")
